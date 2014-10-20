66930568e92754144ae109b5c22c7e223386fed5
Dan Sheard <dan.c.sheard@gmail.com>
2014-10-20 18:57:29 +0100

    Add feedback to user on which number they've clicked

--- myApp.js
+++ myApp.js
@@ -1,35 +1,42 @@
 angular.module('myApp', [])
-.controller('MultiplicationCtrl', function($scope, $attrs) {
-    function populateNumbers(x) {
-        var numbers = [];
-        for(var i=0; i<x; i++) {
-            numbers[i] = i + 1; 
-        };
-        return numbers;
-    }
+  .controller('DisplayCtrl', function($scope) {
+			$scope.$on('displayData', function(event, data) {
+				$scope.content = data;
+			});
+		})
+.controller('MultiplicationCtrl', function($scope, $attrs, $rootScope) {
+	function populateNumbers(x) {
+		var numbers = [];
+		for(var i=0; i<x; i++) 
+    {numbers[i] = i + 1; };
+    return numbers;
+	}
 
-    $scope.compute = function(a,b) {
-        return a * b;
-    }
+	$scope.compute = function(a,b) {
+     return a * b;
+  };
 
-    $scope.$watch('numberLimit', function(limit) {
-        $scope.numbers = populateNumbers(limit);
-    });
+  $scope.$watch("numberLimit", function(limit) {
+	  $scope.numbers = populateNumbers(limit);
+  });
 
-    $scope.numberLimit = $attrs.initialNumberLimit || 10;
+  $scope.numberLimit = $attrs.initialNumberLimit || 10;
+  
+	var activeFactorA, activeFactorB;
+	$scope.setActiveFactors = function(a, b) {
+		activeFactorA = a;
+		activeFactorB = b;
+	};
 
-    var activeFactorA, activeFactorB;
-    $scope.setActiveFactors = function(a, b) {
-        activeFactorA = a;
-        activeFactorB = b;
-    };
-
-    $scope.clearActiveFactors = function() {
-    	activeFactorA = activeFactorB = null;
-    };
-
-    $scope.matchesFactor = function (a, b) {
-        return a === activeFactorA || b === activeFactorB;
-    };
+	$scope.matchesFactor = function(a, b) {
+		return a === activeFactorA || b === activeFactorB;
+	};
+  
+  $scope.setActiveNumber = function(number) {
+		$rootScope.$broadcast('displayData', number);
+	};
+    
+  $scope.clearActiveFactors = function() {
+	  activeFactorA = activeFactorB = null;
+	};
 });
-
